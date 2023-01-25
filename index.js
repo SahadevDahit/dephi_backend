3c080354dc54ce5a20607d5e96315269a14fff5d
Sahadev Dahit <sahadevdahit123@gmail.com>
2023-01-25 16:58:47 +0545

    Update index.js

--- index.js
+++ index.js
@@ -3,26 +3,17 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
-
 dotenv.config();
-app.use(cors({
-    origin: '*',
-    credentials: true
-}))
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With,  Accept"
-    );
-    res.setHeader('Content-Type', 'application/json')
-    res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET, POST, PATCH, DELETE, OPTIONS"
-    );
-    next();
-});
-// Connect DB
+
+app.use(express.json());
+const corsOptions = {
+  origin: '*',
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+}
+
+app.use(cors(corsOptions))
+app.use(cors())
 mongoose
   .connect(process.env.MONGO_URL, {
     useCreateIndex: true,
@@ -34,8 +25,7 @@ mongoose
   .catch((err) => console.log(err));
 
 // Middleware
-app.use(express.json());
-app.use(cors());
+
 // Route
 app.use("/dphi", require("./routes/dphi"));
 app.get('/', (req, res) => {
@@ -43,6 +33,6 @@ app.get('/', (req, res) => {
 })
 
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 4000, () => {
   console.log("App is running")
 });
